8b936efab28cf2f762bfe6778ec733cffa4d4540
Dmitry Shishlyannikov <diman0497@mail.ru>
2024-01-16 14:20:37 -0500

    [BOP-217] Make provider names externally reusable (#37)

--- internal/distro/provider.go
+++ internal/distro/provider.go
@@ -4,16 +4,8 @@ import (
 	"fmt"
 
 	"github.com/mirantiscontainers/boundless-cli/internal/k8s"
+	"github.com/mirantiscontainers/boundless-cli/pkg/constants"
 	"github.com/mirantiscontainers/boundless-cli/pkg/types"
-)
-
-const (
-	// ProviderK0s is the name of the k0s provider
-	ProviderK0s = "k0s"
-	// ProviderKind is the name of the kind provider
-	ProviderKind = "kind"
-	// ProviderExisting is the name of the existing provider
-	ProviderExisting = "existing"
 )
 
 // Provider is the interface for a distro provider
@@ -26,9 +18,9 @@ type Provider interface {
 // GetProvider returns a new provider
 func GetProvider(blueprint *types.Blueprint, kubeConfig *k8s.KubeConfig) (Provider, error) {
 	switch blueprint.Spec.Kubernetes.Provider {
-	case ProviderK0s:
+	case constants.ProviderK0s:
 		return NewK0sProvider(blueprint, kubeConfig), nil
-	case ProviderKind:
+	case constants.ProviderKind:
 		return NewKindProvider(blueprint, kubeConfig), nil
 	default:
 		return nil, fmt.Errorf("invalid kubernetes distribution provider: %s", blueprint.Spec.Kubernetes.Provider)
